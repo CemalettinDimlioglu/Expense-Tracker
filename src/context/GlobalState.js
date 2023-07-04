13de779d0b63cb96c83ac7d48c40e092e53f8a25
CMLTTN <cemaldim@gmail.com>
2023-07-04 12:44:52 +0300

    Expense Trucker done

---
+++ src/context/GlobalState.js
@@ -0,0 +1,44 @@
+import React, {createContext,useReducer} from 'react';
+import AppReducer from './AppReducer';
+
+
+// Inıtial state
+
+const initialState= {
+
+transactions:[
+  
+]
+
+} 
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider Component
+export const GlobalProvider = ({children})=> {
+
+     const [state, dispatch]= useReducer(AppReducer, initialState);
+
+     function deleteTransaction(id){
+    dispatch({
+     type: 'DELETE_TRANSACTION',
+     payload:id
+    });
+    }
+     function addTransaction(transaction){
+    dispatch({
+     type: 'ADD_TRANSACTION',
+     payload:transaction
+    });
+    }
+
+     return (<GlobalContext.Provider value={{
+     transactions:state.transactions,
+     deleteTransaction,
+     addTransaction
+     }}>
+        {children}  
+     </GlobalContext.Provider>);
+
+}
